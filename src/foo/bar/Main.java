811706a9097b9d3d8ac791c54ec43eca035c62b4
Nishikant Ajay Patil <crusnik02.trinityblood@gmail.com>
2015-10-14 23:41:54 +0530

    removed unused code

--- src/foo/bar/Main.java
+++ src/foo/bar/Main.java
@@ -6,10 +6,10 @@ public class Main {
 
     public static void main(String[] args) {
         Map<String, MyValue> myMap = new MyMap();
-        MyValue val1 = new MyValue("x", 11);
+        MyValue val1 = new MyValue("x", 1);
         MyValue val2 = new MyValue("y", 2);
         myMap.put("a", val1);
-        myMap.put("b", val2);
+        myMap.put("a", val2);
         System.out.println(myMap);
     }
 }
--- src/foo/bar/MyValue.java
+++ src/foo/bar/MyValue.java
@@ -4,15 +4,15 @@ package foo.bar;
  * Created by Nishikant on 10/14/2015.
  */
 public class MyValue {
-    private String name;
+    private String value;
     private int revision;
 
-    public String getName() {
-        return name;
+    public String getValue() {
+        return value;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setValue(String value) {
+        this.value = value;
     }
 
     public int getRevision() {
@@ -23,29 +23,15 @@ public class MyValue {
         this.revision = revision;
     }
 
-    public MyValue(String name, int revision) {
-        this.name = name;
+    public MyValue(String value, int revision) {
+        this.value = value;
         this.revision = revision;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MyValue myValue = (MyValue) o;
-        return this.name.equals(this.name);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name.hashCode();
-        return result;
     }
 
     @Override
     public String toString() {
         return "MyValue{" +
-                "name=" + name +
+                "value=" + value +
                 ", revision=" + revision +
                 '}';
     }
